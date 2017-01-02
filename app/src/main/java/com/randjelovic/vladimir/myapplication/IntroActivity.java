5f57a54dad4581f93cc36097a956dbda25c9ad75
Vladimir <vladimirmicko@gmail.com>
2017-01-02 13:30:58 +0100

    Created Intro, Login and Main activities

---
+++ app/src/main/java/com/randjelovic/vladimir/myapplication/IntroActivity.java
@@ -0,0 +1,26 @@
+package com.randjelovic.vladimir.myapplication;
+
+import android.content.Intent;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.widget.Toast;
+
+public class IntroActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_intro);
+
+        new Handler().postDelayed(new Runnable(){
+            @Override
+            public void run() {
+                 Intent mainIntent = new Intent(IntroActivity.this,Login.class);
+                IntroActivity.this.startActivity(mainIntent);
+                IntroActivity.this.finish();
+            }
+        }, getResources().getInteger(R.integer.splash_time));
+    }
+}
