d160ecb537cf9e34d49b062f8211997b02d793a2
Vladimir <vladimirmicko@gmail.com>
2017-01-14 00:28:31 +0100

    Slide and Test mapping work

--- app/src/main/java/com/randjelovic/vladimir/myapplication/activities/GetTestWithMapper.java
+++ app/src/main/java/com/randjelovic/vladimir/myapplication/activities/GetTestWithMapper.java
@@ -1,12 +1,14 @@
 package com.randjelovic.vladimir.myapplication.activities;
 
 import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.Toast;
 
 import com.randjelovic.vladimir.myapplication.R;
@@ -25,44 +27,59 @@ import org.springframework.web.client.HttpServerErrorException;
 import org.springframework.web.client.ResourceAccessException;
 import org.springframework.web.client.RestTemplate;
 
+import java.io.ByteArrayInputStream;
 import java.net.HttpURLConnection;
 import java.net.URL;
 import java.util.ArrayList;
 
+import data.models.Slide;
 import data.models.Test;
 
 public class GetTestWithMapper extends AppCompatActivity {
 
-    Button button;
+    Button buttonTest;
+    ImageView image;
+    Button buttonSlide;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_get_test);
-        button = (Button) findViewById(R.id.button);
+        setContentView(R.layout.activity_get_test_with_mapper);
+        image = (ImageView) findViewById(R.id.imageTest);
+        buttonTest = (Button) findViewById(R.id.buttonTest);
+        buttonSlide = (Button) findViewById(R.id.buttonSlide);
 
-        button.setOnClickListener(new View.OnClickListener() {
+        buttonTest.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                new GetTestWithMapper.GetTestService().execute();
+                new GetTestWithMapper.GetTestService().execute("test");
+            }
+        });
+
+        buttonSlide.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                new GetTestWithMapper.GetTestService().execute("slide");
             }
         });
     }
 
-    public class GetTestService extends AsyncTask<String, Integer, ResponseEntity<Test>> {
+    public class GetTestService extends AsyncTask<String, Integer, ResponseEntity<Slide>> {
 
-        private static final String TAG = "GetTest";
-        private static final String AUTHENTICATION_TAG = "Basic ";
+        private static final String TAG = "GetTestWithMapper";
         private static final String AUTHENTICATION_HEADER = "Authorization";
-        private Bitmap image;
-        URL url;
+        private Bitmap tempImage;
+        URL urlTest;
+        URL urlSlide;
 
         @Override
-        protected ResponseEntity<Test> doInBackground(String... strings) {
+        protected ResponseEntity doInBackground(String... strings) {
             publishProgress(0);
             String message = null;
             try {
-                url = new URL("http://10.0.2.2:8092/PMAspring/rest/tests");
+                urlTest = new URL("http://10.0.2.2:8092/PMAspring/rest/tests");
+                urlSlide = new URL("http://10.0.2.2:8092/PMAspring/rest/tests/slides");
             }
             catch (Exception e){
             }
@@ -72,27 +89,29 @@ public class GetTestWithMapper extends AppCompatActivity {
             requestHeaders.set(AUTHENTICATION_HEADER, MyApplication.getBasicAuth());
             requestHeaders.setContentType(MediaType.APPLICATION_JSON);
             HttpEntity<?> requestEntity = new HttpEntity<Object>(requestHeaders);
-
             RestTemplate restTemplate = new RestTemplate(true);
             restTemplate.getMessageConverters().add(new MappingJackson2HttpMessageConverter());
 
-            ResponseEntity<Test> response;
+            ResponseEntity response=null;
 
             try {
-                response = restTemplate.exchange("http://10.0.2.2:8092/PMAspring/rest/tests/1", HttpMethod.GET, requestEntity, Test.class);
+                if(strings[0].equals("test")) response = restTemplate.exchange("http://10.0.2.2:8092/PMAspring/rest/tests/1", HttpMethod.GET, requestEntity, Test.class);
+                if(strings[0].equals("slide")) response = restTemplate.exchange("http://10.0.2.2:8092/PMAspring/rest/tests/slides/1", HttpMethod.GET, requestEntity, Slide.class);
             }
             catch(HttpClientErrorException e){
                 throw e;
             }
             catch(ResourceAccessException e){
-                Log.v("getData() ", "exception "+ e.getMessage());
+                Log.v(TAG, "exception "+ e.getMessage());
                 throw e;
             }catch(HttpServerErrorException e)
             {
-                Log.v("getData() ", "exception "+ e.getMessage());
+                Log.v(TAG, "exception "+ e.getMessage());
                 throw e;
             }
+
             return response;
+
         }
 
         @Override
@@ -102,10 +121,15 @@ public class GetTestWithMapper extends AppCompatActivity {
         }
 
         @Override
-        protected void onPostExecute(ResponseEntity<Test> result) {
-            super.onPostExecute(result);
-            Log.d(TAG, "GET - Result: " + result);
-            Toast.makeText(MyApplication.getAppContext(), "Slika je prikazana: "+result.getBody().getTestName(), Toast.LENGTH_LONG).show();
+        protected void onPostExecute(ResponseEntity<Slide> response) {
+            super.onPostExecute(response);
+            Log.d(TAG, "GET - Result: " + response);
+
+
+            Toast.makeText(MyApplication.getAppContext(), "Slika je prikazana: "+response.getBody().getSlideName(), Toast.LENGTH_LONG).show();
+            tempImage = BitmapFactory.decodeStream(new ByteArrayInputStream(response.getBody().getPrimingImage()));
+            ImageView iii = GetTestWithMapper.this.image;
+            iii.setImageBitmap(tempImage);
         }
     }
 
