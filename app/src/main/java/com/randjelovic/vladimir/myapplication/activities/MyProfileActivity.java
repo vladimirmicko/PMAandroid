a874f802ab89db1f1cd7285901e39d6d9a87ad8f
Vladimir Randjelovic <72micko@gmail.com>
2018-05-26 22:32:45 +0200

    Code refactoring

--- app/src/main/java/com/randjelovic/vladimir/myapplication/activities/MyProfileActivity.java
+++ app/src/main/java/com/randjelovic/vladimir/myapplication/activities/MyProfileActivity.java
@@ -10,6 +10,6 @@ public class MyProfileActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_my_profile2);
+        setContentView(R.layout.activity_my_profile);
     }
 }
